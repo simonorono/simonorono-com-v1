aab14250a129d1b683237721c9f809fdd5f4aab3
Simón Oroño <simonorono@protonmail.com>
2021-12-12 07:49:39 -0400

    Update dependencies
    TailwindCSS was updated to version 3.0.1 so its configuration file was
    updated accordingly.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,5 @@
 module.exports = {
-  purge: [
+  content: [
     './index.html'
   ],
-  mode: 'jit',
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
