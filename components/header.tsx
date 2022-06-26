ed32f01dc65abec3780af4d18c23671b60f010b0
Simón Oroño <simonorono@protonmail.com>
2022-06-26 17:16:41 -0400

    Make the header images link to index

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import Bio from './bio'
 import FixedSizeImage from './fixed-size-image'
 import ProfileLinks from './profile-links'
@@ -6,27 +7,31 @@ import ProfileLinks from './profile-links'
 export default function Header() {
   return (
     <div className="h-card flex flex-col items-center justify-center space-y-8 border-b border-slate-300 bg-slate-100 p-10 shadow-lg lg:fixed lg:inset-y-0 lg:w-96 lg:justify-start lg:border-b-0 lg:border-r">
-      {process.env.NEXT_PUBLIC_GRAVATAR_HASH && (
-        <FixedSizeImage
-          src={`https://www.gravatar.com/avatar/${process.env.NEXT_PUBLIC_GRAVATAR_HASH}.jpg?s=250`}
-          alt="Simón Oroño's picture"
-          width={250}
-          height={250}
-          imageClassName="u-photo rounded-2xl border border-slate-300 bg-slate-200"
-          containerClassName="mx-auto hidden sm:mx-0 lg:block"
-        />
-      )}
+      <Link passHref href="/">
+        <a className="block">
+          {process.env.NEXT_PUBLIC_GRAVATAR_HASH && (
+            <FixedSizeImage
+              src={`https://www.gravatar.com/avatar/${process.env.NEXT_PUBLIC_GRAVATAR_HASH}.jpg?s=250`}
+              alt="Simón Oroño's picture"
+              width={250}
+              height={250}
+              imageClassName="u-photo rounded-2xl border border-slate-300 bg-slate-200"
+              containerClassName="mx-auto hidden sm:mx-0 lg:block"
+            />
+          )}
 
-      {process.env.NEXT_PUBLIC_GRAVATAR_HASH && (
-        <FixedSizeImage
-          src={`https://www.gravatar.com/avatar/${process.env.NEXT_PUBLIC_GRAVATAR_HASH}.jpg?s=200`}
-          alt="Simón Oroño's picture"
-          width={200}
-          height={200}
-          imageClassName="u-photo rounded-2xl border border-slate-300 bg-slate-200"
-          containerClassName="mx-auto sm:mx-0 lg:hidden"
-        />
-      )}
+          {process.env.NEXT_PUBLIC_GRAVATAR_HASH && (
+            <FixedSizeImage
+              src={`https://www.gravatar.com/avatar/${process.env.NEXT_PUBLIC_GRAVATAR_HASH}.jpg?s=200`}
+              alt="Simón Oroño's picture"
+              width={200}
+              height={200}
+              imageClassName="rounded-2xl border border-slate-300 bg-slate-200"
+              containerClassName="mx-auto sm:mx-0 lg:hidden"
+            />
+          )}
+        </a>
+      </Link>
 
       <a href="/" className="text-center lg:w-full lg:text-left">
         <h1 className="p-name text-xl font-medium">Simón Oroño</h1>
