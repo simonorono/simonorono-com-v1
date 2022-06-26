c4da4a9df95967405f18bbb96ab4928c199fd868
Simón Oroño <simonorono@protonmail.com>
2022-06-26 16:40:21 -0400

    Add h-card classes

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ import ProfileLinks from './profile-links'
 export default function Header() {
   return (
     <div
-      className="flex flex-col items-center justify-center space-y-8 border-b bg-slate-100 p-10 shadow lg:fixed lg:inset-y-0 lg:w-96 lg:justify-start lg:border-b-0 lg:border-r">
+      className="h-card flex flex-col items-center justify-center space-y-8 border-b bg-slate-100 p-10 shadow lg:fixed lg:inset-y-0 lg:w-96 lg:justify-start lg:border-b-0 lg:border-r">
       {process.env.NEXT_PUBLIC_GRAVATAR_HASH && (
         <FixedSizeImage
           src={`https://www.gravatar.com/avatar/${process.env.NEXT_PUBLIC_GRAVATAR_HASH}.jpg?s=250`}
@@ -30,14 +30,14 @@ export default function Header() {
       )}
 
       <a href="/" className="text-center lg:w-full lg:text-left">
-        <h1 className="text-xl font-medium">Simón Oroño</h1>
+        <h1 className="p-name text-xl font-medium">Simón Oroño</h1>
       </a>
 
       <div className="flex justify-center space-x-6 lg:hidden">
         <ProfileLinks />
       </div>
 
-      <article className="prose prose-slate hidden grow lg:block">
+      <article className="p-note prose prose-slate hidden grow lg:block">
         <Bio />
       </article>
 
