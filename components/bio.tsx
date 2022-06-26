3e0c878399e8aefdd1dc171cd03001d74306e467
Simón Oroño <simonorono@protonmail.com>
2022-06-26 14:23:46 -0400

    Create bio component
    Will be used in the new header and footer.

---
+++ components/bio.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Bio() {
+  return (
+    <>
+      <p>
+        I'm a computer scientist and software developer from Maracaibo,
+        Venezuela. I work as a full-stack web developer.
+      </p>
+
+      <p>
+        My interests are programming languages, graph theory, discrete
+        mathematics and video games.
+      </p>
+    </>
+  )
+}
