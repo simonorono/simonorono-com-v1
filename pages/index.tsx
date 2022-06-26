66fd86fb9212d2cd830bef19389adedb2904c0d9
Simón Oroño <simonorono@protonmail.com>
2022-06-26 16:31:13 -0400

    Display bio at index rather than at the header on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Bio from '../components/bio'
 import Layout from '../components/layout'
 import ProjectCard from '../components/project-card'
 import projectData from '../data/projects.json'
@@ -9,13 +10,19 @@ export default function Index() {
   return (
     <Layout>
       <div className="space-y-8">
-        <h2 className="text-2xl font-medium">Projects</h2>
+        <div className="prose">
+          <div className="lg:hidden">
+            <Bio />
+          </div>
 
-        <p className="prose prose-neutral">
-          Besides writing code for a living, I also like writing code for
-          recreational purposes. Here's a list of projects I've written in my
-          free time.
-        </p>
+          <h2 className="text-2xl font-medium">Projects</h2>
+
+          <p>
+            Besides writing code for a living, I also like writing code for
+            recreational purposes. Here's a list of projects I've written in my
+            free time.
+          </p>
+        </div>
 
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
           {projects.map(project => (
