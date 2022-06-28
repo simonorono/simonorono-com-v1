c9c7cfef8a9cd8b477fcf312d8d4f142cb8836a2
Simón Oroño <simonorono@protonmail.com>
2022-06-28 08:04:42 -0400

    Refresh project card component borders

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -11,7 +11,7 @@ export default function ProjectCard({ project }: Props) {
   const { name, description, tags, url, vcs } = project
 
   return (
-    <div className="flex flex-col space-y-4 border border-gray-400 p-4">
+    <div className="flex flex-col space-y-4 rounded-xl border border-slate-300 p-4">
       <h3 className="text-2xl font-medium">{name}</h3>
 
       <div className="flex flex-col items-baseline space-y-4 sm:space-y-2">
@@ -30,7 +30,7 @@ export default function ProjectCard({ project }: Props) {
         </NewTabLink>
       </div>
 
-      <p className="flex-grow pt-2">{description}</p>
+      <p className="prose flex-grow pt-2">{description}</p>
 
       <div className="relative bottom-0 space-y-2">
         {tags.map(tag => (
