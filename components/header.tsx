889699f1eb70d167e28649e6ae3eb9da1131515c
Simón Oroño <simonorono@protonmail.com>
2022-07-03 18:16:53 -0400

    Use better responsive images for the profile picture
    Now using the `picture` tag.

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Bio from './bio'
-import FixedSizeImage from './fixed-size-image'
+import ProfilePicture from './profile-picture'
 import ProfileLinks from './profile-links'
 
 function name(h1: boolean) {
@@ -18,27 +18,7 @@ export default function Header() {
     <div className="h-card flex flex-col items-center justify-center space-y-8 border-b border-slate-300 bg-slate-100 p-10 shadow-lg lg:fixed lg:inset-y-0 lg:w-96 lg:justify-start lg:border-b-0 lg:border-r">
       <Link passHref href="/">
         <a className="u-url u-uid block" rel="me">
-          {process.env.NEXT_PUBLIC_GRAVATAR_HASH && (
-            <FixedSizeImage
-              src={`https://www.gravatar.com/avatar/${process.env.NEXT_PUBLIC_GRAVATAR_HASH}.jpg?s=250`}
-              alt="Simón Oroño's picture"
-              width={250}
-              height={250}
-              imageClassName="u-photo rounded-2xl border border-slate-300 bg-slate-200"
-              containerClassName="mx-auto hidden sm:mx-0 lg:block"
-            />
-          )}
-
-          {process.env.NEXT_PUBLIC_GRAVATAR_HASH && (
-            <FixedSizeImage
-              src={`https://www.gravatar.com/avatar/${process.env.NEXT_PUBLIC_GRAVATAR_HASH}.jpg?s=200`}
-              alt="Simón Oroño's picture"
-              width={200}
-              height={200}
-              imageClassName="rounded-2xl border border-slate-300 bg-slate-200"
-              containerClassName="mx-auto sm:mx-0 lg:hidden"
-            />
-          )}
+          <ProfilePicture />
         </a>
       </Link>
 
---
+++ components/profile-picture.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const DESKTOP_SQUARE_SIDE = 250
+const MOBILE_SQUARE_SIDE = 200
+
+export default function ProfilePicture() {
+  const imageUrl = process.env.NEXT_PUBLIC_GRAVATAR_HASH
+
+  return (
+    <>
+      {imageUrl && (
+        <div className="h-[200px] w-[200px] rounded-2xl border border-slate-300 bg-slate-200 lg:h-[250px] lg:w-[250px]">
+          <picture>
+            <source
+              media="(max-width: 1023px)"
+              srcSet={`https://www.gravatar.com/avatar/${imageUrl}.jpg?s=${MOBILE_SQUARE_SIDE}`}
+            />
+            <source
+              media="(min-width: 1024px)"
+              srcSet={`https://www.gravatar.com/avatar/${imageUrl}.jpg?s=${DESKTOP_SQUARE_SIDE}`}
+            />
+            <img
+              src={`https://www.gravatar.com/avatar/${imageUrl}.jpg?s=${DESKTOP_SQUARE_SIDE}`}
+              alt="Simón Oroño's profile picture"
+              className="u-photo"
+            />
+          </picture>
+        </div>
+      )}
+    </>
+  )
+}
