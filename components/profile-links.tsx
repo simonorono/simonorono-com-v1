2e97c948603168e13619311e4da901097e0a9ad9
Simón Oroño <simonorono@protonmail.com>
2022-07-12 07:44:54 -0400

    Add StackOverflow icon to profile links

--- components/profile-links.tsx
+++ components/profile-links.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import LinkedinIcon from './icons/linkedin-icon'
 import GithubIcon from './icons/github-icon'
+import StackOverflowIcon from './icons/stack-overflow-icon'
 import TwitterIcon from './icons/twitter-icon'
 
 export default function ProfileLinks() {
@@ -24,6 +25,16 @@ export default function ProfileLinks() {
       >
         <span className="sr-only">github profile (opens in new window)</span>
         <GithubIcon className="h-10 w-10" />
+      </a>
+
+      <a
+        href="https://stackoverflow.com/users/3001663/sim%c3%b3n-oro%c3%b1o"
+        className="inline-block h-10 w-10"
+        target="_blank"
+        rel="me"
+      >
+        <span className="sr-only">stack overflow profile (opens in new window)</span>
+        <StackOverflowIcon className="h-10 w-10" />
       </a>
 
       <a
