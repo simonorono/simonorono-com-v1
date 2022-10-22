63e4fd3949d71eedba398370bd17e0c48e08a219
Simón Oroño <simonorono@protonmail.com>
2022-10-22 09:04:35 -0400

    Remove unused import

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { CodeBracketSquareIcon, LinkIcon } from '@heroicons/react/24/outline'
 import Link from './link'
-import Tag from './tag'
 
 interface Props {
   project: Project
