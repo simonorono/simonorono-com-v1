ebbc1fdd381f3600099631a418080ca351f80a5f
Simón Oroño <simonorono@protonmail.com>
2022-10-22 08:50:08 -0400

    Remove Nuxt version of page
    I didn't like Nuxt

--- nxt/nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
-  runtimeConfig: {
-    public: {
-      gravatarHash: '',
-    },
-  },
-})
