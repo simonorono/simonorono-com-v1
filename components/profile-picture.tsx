600b6d2038677cbfd5fdee54348b23b7d224cf2e
Simón Oroño <simonorono@protonmail.com>
2022-10-22 09:18:07 -0400

    Add explicit sizes to images

--- components/profile-picture.tsx
+++ components/profile-picture.tsx
@@ -12,14 +12,20 @@ export default function ProfilePicture() {
         <div className="h-[200px] w-[200px] overflow-hidden rounded-2xl border border-slate-300 bg-slate-200 lg:h-[250px] lg:w-[250px]">
           <picture>
             <source
+              height={MOBILE_SQUARE_SIDE}
+              width={MOBILE_SQUARE_SIDE}
               media="(max-width: 1023px)"
               srcSet={`https://www.gravatar.com/avatar/${imageUrl}.jpg?s=${MOBILE_SQUARE_SIDE}`}
             />
             <source
+              height={DESKTOP_SQUARE_SIDE}
+              width={DESKTOP_SQUARE_SIDE}
               media="(min-width: 1024px)"
               srcSet={`https://www.gravatar.com/avatar/${imageUrl}.jpg?s=${DESKTOP_SQUARE_SIDE}`}
             />
             <img
+              height={DESKTOP_SQUARE_SIDE}
+              width={DESKTOP_SQUARE_SIDE}
               src={`https://www.gravatar.com/avatar/${imageUrl}.jpg?s=${DESKTOP_SQUARE_SIDE}`}
               alt="Simón Oroño's profile picture"
               className="u-photo"
